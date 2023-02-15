2a044a17adc515b4b5f672c7613dc7ac9f729e0e
Sasha <alex_penza@list.ru>
2023-02-15 16:55:32 +0300

    simple functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,35 @@
+
+// == Strings ==
+
 const val name = "Alex"
 val greeting = null
 
+// == functions ==
+fun print(): String {
+    return "Hello $name"
+}
+
+fun printNull(): String? {
+    return null
+}
+
+//single expression function
+fun print2(): String = "Hello"
+
+fun print3(strVar:String){
+    var s = strVar + "Hello"
+    println(s)
+}
+
 fun main() {
-    val s = if(greeting!=null) greeting else "Hi"
 
-
+    // == Strings ==
+    val s = when (greeting){
+        null -> "Hi"
+        else -> greeting
+    }
     println("$s $name")
+
+    // == functions ==
+    println(print())
 }
